ddbb962d5fe5755d5033a3590c75d2a04a17e445
Alex Xiong <alex.xiong.tech@gmail.com>
2021-09-30 08:58:28 +0800

    fix: demo for bug

--- test/contracts/mock/Counter.sol
+++ test/contracts/mock/Counter.sol
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import 'hardhat/console.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
 contract Counter {
   address public deployer;
   uint256 public count;
+
+  IERC20 private _dai;
 
   constructor(uint256 _startAt) {
     count = _startAt;
@@ -13,4 +19,17 @@ contract Counter {
   function add(uint256 _amount) external {
     count += _amount;
   }
+
+  function setDaiAddress(address _daiAddress) external {
+    _dai = IERC20(_daiAddress);
+  }
+
+  function payDaiToAdd(uint256 _amount) external {
+    count += _amount;
+    console.log('from contract: calling transferFrom');
+    _dai.transferFrom(msg.sender, address(this), _amount);
+    console.log('from contract: finish transferFrom');
+  }
 }
+
+contract Dai is ERC20('Dai Stablecoin', 'DAI') {}
--- test/unit/mock/call-through.spec.ts
+++ test/unit/mock/call-through.spec.ts
@@ -1,6 +1,8 @@
 import { MockContract, MockContractFactory, smock } from '@src';
-import { Counter, Counter__factory } from '@typechained';
+import { Counter, Counter__factory, Dai, Dai__factory } from '@typechained';
 import chai, { expect } from 'chai';
+import { waffle, ethers } from 'hardhat';
+import CounterABI from '../../../artifacts/test/contracts/mock/Counter.sol/Counter.json';
 
 chai.should();
 chai.use(smock.matchers);
@@ -35,4 +37,70 @@ describe('Mock: Call through', () => {
     await mock.add(10);
     expect(mock.add).to.be.calledOnceWith(10);
   });
+
 });
+
+describe('waffle deploy v.s. ethers deploy', () => {
+  let counter: Counter;
+  let daiFactory: MockContractFactory<Dai__factory>;
+  let daiMock: MockContract<Dai>;
+  let [alice] = waffle.provider.getWallets();
+
+  before(async () => {
+    daiFactory = await smock.mock('Dai');
+  })
+
+  describe('deploy using waffle', () => {
+    beforeEach(async () => {
+      counter = (await waffle.deployContract(alice, CounterABI, [1])) as Counter;
+      daiMock = await daiFactory.deploy();
+
+      await counter.setDaiAddress(daiMock.address);
+    })
+
+    it('should have correct call count', async () => {
+      const amount = 5;
+      expect(daiMock.transferFrom).to.have.callCount(0);
+
+      await daiMock.setVariable('_balances', {
+        [alice.address]: amount,
+      });
+      await daiMock.setVariable('_allowances', {
+        [alice.address]: {
+          [counter.address]: amount,
+        },
+      });
+
+      await expect(counter.payDaiToAdd(amount)).to.not.be.reverted;
+      // FIXME: the callCount is 12 !! Whaaaat?
+      expect(daiMock.transferFrom).to.have.callCount(1);
+    })
+  })
+
+  describe('deploy using ethers', () => {
+    beforeEach(async () => {
+      const counterFactory = await ethers.getContractFactory('Counter');
+      counter = await counterFactory.deploy(1);
+      daiMock = await daiFactory.deploy();
+
+      await counter.setDaiAddress(daiMock.address);
+    })
+    it('should have correct call count', async () => {
+      const amount = 5;
+      expect(daiMock.transferFrom).to.have.callCount(0);
+
+      await daiMock.setVariable('_balances', {
+        [alice.address]: amount,
+      });
+      await daiMock.setVariable('_allowances', {
+        [alice.address]: {
+          [counter.address]: amount,
+        },
+      });
+
+      await expect(counter.payDaiToAdd(amount)).to.not.be.reverted;
+      // NOTE: correctly counted
+      expect(daiMock.transferFrom).to.have.callCount(1);
+    })
+  })
+})
